2aa38033a9efae2d0d8f747e112bee1b88dd092c
Rifhice <giordani.kevin.02@gmail.com>
2018-08-25 01:23:56 +0000

    update start.sh

--- log.d
+++ log.d
@@ -10,3 +10,1252 @@ Send update to main server =>
 Send update to main server => 
 <Buffer 31 65 63 33 62 32 62 61 2d 61 35 35 38 2d 31 31 65 38 2d 39 38 64 30 2d 35 32 39 32 36 39 66 62 31 34 35 39 2f 23 2f 74 72 75 65>
 Python sensor disconnected !
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Error
+Connection closed
+Connection closed
+Connection success
+Connection success
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+processId=$(ps -ef | grep 'indexPresence' | grep -v 'grep' | awk '{ printf $2 }')
+sudo kill -9 $processId
 cd /home/pi/Desktop/Hom-E_Presence_Sensor/
 git pull
-sudo node index.js
+node indexPresence.js
